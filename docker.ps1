20be8afd86c46992350145ce07601f2963083f92
Oleksii Morozov <95098796+Oleksii-Morozov@users.noreply.github.com>
2024-02-08 23:33:40 +0200

    feat: modify script to remove old containers and image on rebuild

--- docker.ps1
+++ docker.ps1
@@ -1,16 +1,31 @@
 # Set the image name
 $imageName = "kb_hackathon_test_b"
 
+# Find containers running from the image
+$runningContainers = docker ps -q --filter ancestor=$imageName
+
+if ($runningContainers) {
+    Write-Host "Stopping running containers from image $imageName."
+    # Stop the running containers
+    docker stop $runningContainers
+    Write-Host "Removing stopped containers."
+    # Remove the stopped containers
+    docker rm $runningContainers
+}
+
 # Check if the Docker image already exists
 $imageExists = docker image ls -q $imageName
 
 if ($imageExists) {
-    Write-Host "Image $imageName exists. Proceeding with force rebuild."
+    Write-Host "Image $imageName exists. Removing the existing image before rebuilding."
+    # Remove the existing Docker image
+    docker rmi $imageName -f
+    Write-Host "Image $imageName has been removed."
 } else {
-    Write-Host "Image $imageName does not exist. Building a new image."
+    Write-Host "Image $imageName does not exist. Proceeding with the build."
 }
 
-# Build (or rebuild) the Docker image
+# Build the Docker image
 docker build -t $imageName .
 
 # Check for success of the build process
